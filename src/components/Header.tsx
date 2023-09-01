db46b1e4a2d9d156bfa67c0f30bc76767e240f05
lishu <lishugupta652@gmail.com>
2023-09-02 03:47:00 +0530

    major: logout added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,27 +7,55 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import useUser from '../hooks/useUser'
 
 
 const Header = () => {
+    const { isUserDataLoading, isUserLoggedIn, userData } = useUser();
+
+    const logout = () => {
+        localStorage.removeItem('auth_token');
+        window.location.reload();
+    }
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Geo Attend
+                            <Link to="/">
+                                Geo Attend
+                            </Link>
                         </Typography>
                         <Button color="inherit">
                             <Link to="/">Home</Link>
                         </Button>
-                        <Button color="inherit">
-                            <Link to="/login">Login</Link>
-                        </Button>
-                        <Button color="inherit">
-                            <Link to="/signup">Signup</Link>
-                        </Button>
+
+                        {
+                            isUserLoggedIn && (
+                                <>
+                                    <Button color="inherit">
+                                        <Link to="/dashboard">Dashboard</Link>
+                                    </Button>
+                                    <Button color="inherit">
+                                        <div onClick={logout}>Logout</div>
+                                    </Button>
+                                </>
+                            )
+                        }
+                        {
+                            !isUserDataLoading && !isUserLoggedIn && (
+                                <>
+                                    <Button color="inherit">
+                                        <Link to="/login">Login</Link>
+                                    </Button>
+                                    <Button color="inherit">
+                                        <Link to="/signup">Signup</Link>
+                                    </Button>
+                                </>
+                            )
+                        }
                     </Toolbar>
                 </AppBar>
             </Box>
