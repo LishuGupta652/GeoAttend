8b8657c564cecb4112e07ab693098293735d5e83
lishu <lishugupta652@gmail.com>
2023-09-02 03:27:21 +0530

    Breaking-changes: login signup setup

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -33,3 +33,20 @@ export const signupValidation = (values: any) => {
     }
     return errors;
 }
+export const loginValidation = (values: any) => {
+    const errors = {} as any;
+    if (!values.email) {
+        errors.email = 'Email is required';
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+        errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+        errors.password = 'Password is required';
+    } else if (values.password.length < 6) {
+        errors.password = 'Password must be atleast 6 characters';
+    }
+    return errors;
+}
