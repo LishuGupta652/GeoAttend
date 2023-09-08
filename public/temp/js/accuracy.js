b3adceec41179ceb34839b07d59fd82ad9e9fe2f
lishu <lishugupta652@gmail.com>
2023-09-08 13:01:51 +0530

    locatoin accuracy test

--- public/temp/js/accuracy.js
+++ public/temp/js/accuracy.js
@@ -16,3 +16,60 @@ const polygon = L.polygon(destinationPolygon, { color: 'red' }).addTo(map);
 map.fitBounds(polygon.getBounds());
 // polygon.bindTooltip('Destination ', {});
 polygon.bindTooltip('Nitt department  ', {});
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+let userLocationMarker, accuracyCircle;
+const successCallback = (position) => {
+    const { latitude, longitude, accuracy } = position.coords;
+
+    // remove the marker if already exists
+    if (userLocationMarker) {
+        map.removeLayer(userLocationMarker);
+    }
+
+    userLocationMarker = L.marker([latitude, longitude]).addTo(map);
+
+    // remove the accuracy circle if already exists
+    if (accuracyCircle) {
+        map.removeLayer(accuracyCircle);
+    }
+
+    accuracyCircle = L.circle([latitude, longitude], {
+        color: 'blue',
+        fillColor: '#f03',
+        radius: accuracy
+    }).addTo(map);
+
+    const isInside = isMarkerInsidePolygon(userLocationMarker, polygon);
+
+    if (isInside) {
+        document.querySelector('.message').innerHTML = 'Your are inside the polygon';
+
+        document.querySelector('.coordinates').innerHTML = `Latitude: ${latitude} <br> Longitude: ${longitude}
+    <br> Accuracy : ${accuracy}`;
+    } else {
+        document.querySelector('.message').innerHTML = 'Your are outside the polygon';
+
+        document.querySelector('.coordinates').innerHTML = `Latitude: ${latitude} <br> Longitude: ${longitude} 
+    <br> Accuracy in (Meters)  : ${accuracy}
+    `;
+    }
+};
+
+const errorCallback = (error) => {
+    console.log(error);
+    document.querySelector('.message').innerHTML = '';
+    document.querySelector('.coordinates').innerHTML = '';
+    document.querySelector('.error').innerHTML = error.message;
+};
+
+if (!navigator.geolocation) {
+    console.log("Your browser doesn't support geolocation feature!");
+} else {
+    navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+}
