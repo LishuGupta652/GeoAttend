4714d43928f1e22e702af7174a410f13cdf092a5
lishu <lishugupta652@gmail.com>
2023-10-24 15:50:46 +0530

    feat: docker frontend fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_USER_API_URL = "http://localhost:3001/api/user/";
+export const BASE_USER_API_URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}/api/user/`;
 
 export const GET_API_TEST = "";
 export const API_SIGNUP = "signup";
